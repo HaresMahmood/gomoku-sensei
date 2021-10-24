2f4e28852a110c4e75641b9f7cb7059350be4e11
Hares Mahmood <hmahmood229@gmail.com>
2021-10-24 11:53:05 +0100

    Add new algorithm to check for terminal/win state.

--- script.js
+++ script.js
@@ -2,79 +2,48 @@ var turn = "black";
 var grid = Array.from(Array(15), () => new Array(15));
 
 $(".box").click(function () {
-  if ($(this).find(".piece").length === 0){ 
+  if ($(this).find(".piece").length === 0) {
     var piece = `<div class="piece ${turn}-piece"></div>`;
-    
+
     $(this).append(piece);
-    
+
     let row = $(this).parent().index();
     let column = $(this).index();
-    
+
     grid[row][column] = turn;
-    
+
     checkWin(turn, row, column);
-    
+
     turn = turn == "black" ? "white" : "black";
   }
 });
 
 function checkWin(turn, row, column) {
-  let horizontalCounter = checkHorizontal(turn, row, column);
-  let verticalCounter = checkVertical(turn, row, column);
-  let diagonalCounter = checkDiagonal(turn, row, column);
-  
-  if (horizontalCounter === 5 || verticalCounter === 5 || diagonalCounter === 5) {
-    window.alert(`${turn} wins!`);
-  }
-}
-
-function checkHorizontal(turn, row, column) {
-    // Horizontal check
-  let counter = 0;
-  
-  for (let i = column - 4; i <= column + 5; i++) {
-    counter = turn === grid[row][i - 1] ? counter + 1 : counter;
-  }
-  
-  return counter;
-}
-
-function checkVertical(turn, row, column) {
-    // Horizontal check
-  let counter = 0;
-  
-  for (let i = row - 4; i <= row + 5; i++) {
-    if (grid[i - 1] !== undefined) {
-      counter = turn === grid[i - 1][column] ? counter + 1 : counter;
+  let horizontalWin = false;
+  let verticalWin = false;
+  let diagRightWin = false;
+  let diagLeftWin = false;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      horizontalWin = checkRow(turn, r, c, 0, 1); // Horizontal.
+      verticalWin = checkRow(turn, r, c, 1, 0); // Vertical.
+      diagRightWin = checkRow(turn, r, c, 1, 1); // Diagonal descending right.
+      diagLeftWin = checkRow(turn, r, c, 1, -1); // Diagonal descending left.
     }
   }
-  
-  return counter;
+
+  console.log(horizontalWin || verticalWin || diagRightWin || diagLeftWin);
 }
 
-function checkDiagonal(turn, row, column) {
-  let counter = 0;
-  
-  console.log(`${row}, ${column}`);
-  
-  for (let i = -5; i < 5; i++) {
-    if (grid[row + i] !== undefined && [row + i, column - i] != [row, column]) {
-      counter = turn === grid[row + i][column - i] ? counter + 1 : counter;
-    }
-  }
-  
-  if (counter !== 4) {
-    counter = 0;
-    
-    for (let i = 5; i > -5; i--) {
-      if (grid[row + i] !== undefined && [row + i, column - i] != [row, column]) {
-        counter = turn === grid[row + i][column - i] ? counter + 1 : counter;
+function checkRow(turn, row, column, verticalStep, horizontalStep) {
+  for (let i = 0 ; i <= 5 ; i++) {
+      let r = row + i * verticalStep;
+      let c = column + i * horizontalStep;
+
+      if (r < 0 || c < 0 || r >= grid.length || c > grid[r].length || grid[r][c] !== turn) {
+          return false;
       }
-    }
   }
-  
-  //console.log(`${row + i}, ${column - i}`);
-  console.log(counter);
-  
-  return counter;
+
+  return true;
 }
