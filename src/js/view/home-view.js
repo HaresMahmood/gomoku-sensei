7a3d516ff0715d115d2bbc61da0f94cc7ebd094b
Hares Mahmood <hmahmood229@gmail.com>
2021-11-08 22:59:53 +0000

    Add dropdown menus to home-page.
    - Dropdowns used from Material Design Components for Web.
       + https://github.com/material-components/material-components-web/tree/master/packages/mdc-select

---
+++ src/js/view/home-view.js
@@ -0,0 +1,8 @@
+const MDCSelect = mdc.select.MDCSelect;
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+
+/*
+select.listen('MDCSelect:change', () => {
+    alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+});
+*/
