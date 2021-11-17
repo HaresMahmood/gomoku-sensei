0e76bbcc6a0d81c7e5c0e7e3fd80256c2a70f274
Hares Mahmood <hmahmood229@gmail.com>
2021-11-17 17:50:26 +0000

    Add `DefaultView`-class, which draws components found on every page.
    - Components so far:
       + Navigation menu.
       + Rules-modal.
       + Settings-modal.

--- src/js/view/defaultView.js
+++ src/js/view/defaultView.js
@@ -1,18 +1,33 @@
 export default class DefaultView {
     constructor(page) {
         this.drawNavigation(page);
+        this.drawSettingsModal();
+        this.drawRulesModal();
+
+        this.setNavigationToggleHandler(this.toggleNavigation); // Setting event-handler here, so that only the minimum amount of information is shown to Controller.
+        this.openRulesModalHandler();
+        this.openSettingsModalHandler();
     }
 
     drawNavigation(page) {
+        console.log(page);
         // TODO: Export to `.txt`-file?
-        let navigation = `<nav> <section> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button"> <span class="material-icons-round red__button"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
+        let navigation = `<nav> <section> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button red__button"> <span class="material-icons-round"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
         
         $("body").append(navigation);
     }
 
-    drawSettingsModal() {}
+    drawSettingsModal() {
+        let modal = `<modal id="rules-page"> <iframe src="./src/html/rules.html" frameborder="0" scrolling="no" onload="this.style.height=(this.contentWindow.document.body.scrollHeight)+'px';"> </iframe> </modal>`;
+    
+        $("body").append(modal);
+    }
 
-    drawRulesModal() {}
+    drawRulesModal() {
+        let modal = `<modal id="settings-page"> <iframe src="./src/html/settings.html" frameborder="0" scrolling="no" onload="this.style.height=(this.contentWindow.document.body.scrollHeight)+'px';"> </iframe> </modal>`;
+    
+        $("body").append(modal);
+    }
 
     toggleNavigation() {
         $("nav").toggleClass("visible");
@@ -20,25 +35,24 @@ export default class DefaultView {
         $("body").toggleClass("overlay");
     }
 
-    setNavigationOpenHandler() {
-        $("#menu-button").bind("click", function() {
+    /*--- Events ---*/
 
+    // TODO: #close-menu-button
+    setNavigationToggleHandler(handler) {
+        $("#menu-button, #close-menu-button").bind("click", handler);
+    }
 
-            // Slide back menu on `back` key press.
-            if (window.history && window.history.pushState) {
-                window.history.pushState('forward', null, './#forward'); // Ensure browser doesn't go back in history.
-      
-                $(window).on('popstate', function() {
-                    // Do opposite of above.
-                    $("nav").removeClass("visible");
-                    $("body").children().not("nav, #settings-page").removeClass("overlay");
-                    $("body").removeClass("overlay");
-                });
-            }
+    openRulesModalHandler() {
+        $("#rules-button").bind("click", function() {
+            $("nav").removeClass("visible");
+            $("#rules-page").addClass("visible");
         });
     }
 
-    setNavigationCloseHandler() {
-
+    openSettingsModalHandler() {
+        $("#settings-button").bind("click", function() {
+            $("nav").removeClass("visible");
+            $("#settings-page").addClass("visible");
+        });
     }
 }
