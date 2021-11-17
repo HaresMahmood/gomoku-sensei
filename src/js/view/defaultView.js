a7d8e5d3f123c4450f2d5565336f97202231bcdc
Hares Mahmood <hmahmood229@gmail.com>
2021-11-17 18:18:02 +0000

    Improve navigation-menu.
    - Add `menu-close-button`.
       + With same formatting as `header`.
    - Improve animations.

--- src/js/view/defaultView.js
+++ src/js/view/defaultView.js
@@ -12,7 +12,7 @@ export default class DefaultView {
     drawNavigation(page) {
         console.log(page);
         // TODO: Export to `.txt`-file?
-        let navigation = `<nav> <section> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button red__button"> <span class="material-icons-round"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
+        let navigation = `<nav> <section class="header__container"> <span id="menu-close-button" class="material-icons-round"> menu_open </span> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button red__button"> <span class="material-icons-round"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
         
         $("body").append(navigation);
     }
@@ -39,7 +39,7 @@ export default class DefaultView {
 
     // TODO: #close-menu-button
     setNavigationToggleHandler(handler) {
-        $("#menu-button, #close-menu-button").bind("click", handler);
+        $("#menu-button, #menu-close-button").bind("click", handler);
     }
 
     openRulesModalHandler() {
