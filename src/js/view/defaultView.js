be51ac56a47869c16194e7c48f4d308595764f3b
Hares Mahmood <hmahmood229@gmail.com>
2021-11-18 15:20:14 +0000

    Make modals responsive.
    - Change width and height to viewport dimensions on smaller-screen devices.

--- src/js/view/defaultView.js
+++ src/js/view/defaultView.js
@@ -5,12 +5,15 @@ export default class DefaultView {
         this.drawRulesModal();
 
         this.setNavigationToggleHandler(this.toggleNavigation); // Setting event-handler here, so that only the minimum amount of information is shown to Controller.
-        this.openRulesModalHandler();
-        this.openSettingsModalHandler();
+        this.setRulesModalOpenHandler(this.resizeModal);
+        this.setSettingsModalOpenHandler(this.resizeModal);
+
+        this.setDocumentResizeHandler(this.resizeModal);
     }
 
+    /*--- Miscellaneous ---*/
+
     drawNavigation(page) {
-        console.log(page);
         // TODO: Export to `.txt`-file?
         let navigation = `<nav> <section class="header__container"> <span id="menu-close-button" class="material-icons-round"> menu_open </span> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button red__button"> <span class="material-icons-round"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
         
@@ -18,13 +21,13 @@ export default class DefaultView {
     }
 
     drawSettingsModal() {
-        let modal = `<modal id="rules-page"> <iframe src="./src/html/rules.html" frameborder="0" scrolling="no" onload="this.style.height=(this.contentWindow.document.body.scrollHeight)+'px';"> </iframe> </modal>`;
+        let modal = `<modal id="rules-page"> <iframe src="./src/html/rules.html" frameborder="0" scrolling="no"> </iframe> </modal>`;
     
         $("body").append(modal);
     }
 
     drawRulesModal() {
-        let modal = `<modal id="settings-page"> <iframe src="./src/html/settings.html" frameborder="0" scrolling="no" onload="this.style.height=(this.contentWindow.document.body.scrollHeight)+'px';"> </iframe> </modal>`;
+        let modal = `<modal id="settings-page"> <iframe src="./src/html/settings.html" frameborder="0" scrolling="no"> </iframe> </modal>`;
     
         $("body").append(modal);
     }
@@ -35,6 +38,18 @@ export default class DefaultView {
         $("body").toggleClass("overlay");
     }
 
+    resizeModal(modal) {
+        const windowWidth = $(window).innerWidth();
+        const iframe = $(modal).find("iframe");
+
+        // TODO: Get `550` value from media breakpoint.
+        const height = windowWidth > 550 ? $(iframe).contents().find("body").height() : $(window).innerHeight();
+
+        $(iframe).height(height);
+    }
+
+
+
     /*--- Events ---*/
 
     // TODO: #close-menu-button
@@ -42,17 +57,27 @@ export default class DefaultView {
         $("#menu-button, #menu-close-button").bind("click", handler);
     }
 
-    openRulesModalHandler() {
+    setRulesModalOpenHandler(handler) {
         $("#rules-button").bind("click", function() {
+            handler("#rules-page");
+
             $("nav").removeClass("visible");
             $("#rules-page").addClass("visible");
         });
     }
 
-    openSettingsModalHandler() {
+    setSettingsModalOpenHandler(handler) {
         $("#settings-button").bind("click", function() {
+            handler("#settings-page");
+
             $("nav").removeClass("visible");
             $("#settings-page").addClass("visible");
+        });
+    }
+
+    setDocumentResizeHandler(handler) {
+        $(window).on("resize", function() {
+            handler("modal");
         });
     }
 }
