df5450940a05518f34a304930655c7d5ba4ef13c
Hares Mahmood <hmahmood229@gmail.com>
2021-12-23 15:42:54 +0000

    Add back `Rules`-page modal.

--- src/js/view/defaultView.js
+++ src/js/view/defaultView.js
@@ -1,44 +1,13 @@
 export default class DefaultView {
     constructor(page) {
-        //this.drawNavigation(page);
-        //this.drawSettingsModal();
-        //this.drawRulesModal();
+        this.setRulesModalOpenHandler(this.openModal);
+        this.setSettingsModalOpenHandler(this.openModal);
+        this.setModalLoadHandler(this.resizeModal, this.closeModal);
 
-        //this.setNavigationToggleHandler(this.toggleNavigation); // Setting event-handler here, so that only the minimum amount of information is shown to Controller.
-        
-        //this.setRulesModalOpenHandler(this.openModal);
-        //this.setSettingsModalOpenHandler(this.openModal);
-        //this.setModalLoadHandler(this.resizeModal, this.closeModal);
-
-        //this.setDocumentResizeHandler(this.resizeModal);
+        this.setDocumentResizeHandler(this.resizeModal);
     }
 
-    /*--- Miscellaneous ---*/
-
-    drawNavigation(page) {
-        // TODO: Export to `.txt`-file?
-        let navigation = `<nav> <section class="header__container"> <span id="menu-close-button" class="material-icons-round"> menu_open </span> <h3> ${page} </h3> </section> <section> <div> <h6> Menu </h6> <button class="menu__button red__button"> <span class="material-icons-round"> home </span> <span> Home </span> </button> <button class="menu__button"> <span class="material-icons-round"> gamepad </span> <span> Game </span> </button> <button id="rules-button" class="menu__button"> <span class="material-icons-round"> gavel </span> <span> Rules </span> </button> <button id="settings-button" class="menu__button"> <span class="material-icons-round"> settings </span> <span> Settings </span> </button> </div><div> <h6> actions </h6> <div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> bolt </span> <span> New game </span> </button> <button class="menu__button"> <span class="material-icons-round"> lightbulb </span> <span> Hint </span> </button> </div><div class="row__container"> <button class="menu__button"> <span class="material-icons-round"> undo </span> <span> Undo </span> </button> <button disabled class="menu__button"> <span class="material-icons-round"> redo </span> <span> Redo </span> </button> </div></div></section> </nav>`
-        
-        $("body").append(navigation);
-    }
-
-    drawSettingsModal() {
-        let modal = `<modal id="rules-page"> <iframe src="./src/html/rules.html" frameborder="0" scrolling="no"> </iframe> </modal>`;
-    
-        $("body").append(modal);
-    }
-
-    drawRulesModal() {
-        let modal = `<modal id="settings-page"> <iframe src="./src/html/settings.html" frameborder="0" scrolling="no"> </iframe> </modal>`;
-    
-        $("body").append(modal);
-    }
-
-    toggleNavigation() {
-        $("nav").toggleClass("visible");
-        $("body").children().not("nav, modal").toggleClass("overlay");
-        $("body").toggleClass("overlay");
-    }
+    /*=== Miscellaneous ===*/
 
     resizeModal(modal) {
         const windowWidth = $(window).innerWidth();
@@ -65,12 +34,7 @@ export default class DefaultView {
 
 
 
-    /*--- Events ---*/
-
-    // TODO: #close-menu-button
-    setNavigationToggleHandler(handler) {
-        $("#menu-button, #menu-close-button").on("click", handler);
-    }
+    /*=== Events ===*/
 
     setRulesModalOpenHandler(handler) {
         $("#rules-button").bind("click", function() {
