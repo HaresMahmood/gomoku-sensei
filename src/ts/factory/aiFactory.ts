0085561a705ce8b2ff48b6a5d6d45102a6fe0f1e
Hares Mahmood <hmahmood229@gmail.com>
2022-02-25 19:10:35 +0000

    Rename folders.

---
+++ src/ts/factory/aiFactory.ts
@@ -0,0 +1,32 @@
+import Player from "../player/player.js";
+import Human from "../player/human.js";
+
+import RandomAI from "../player/ai/randomAI.js";
+import KillerAI from "../player/ai/killerAI.js";
+import DynamicAI from "../player/ai/dynamicAI.js";
+
+import StaticNode from "../player/ai/tree/staticNode.js";
+import DynamicNode from "../player/ai/tree/dynamicNode.js";
+
+export default interface Factory {
+    createItem(input: String, player: number): any;
+}
+
+export default class AIFactory implements Factory {
+    createItem(input: String, player: number): Player {
+        //return input.replace(" ", "");
+
+        if (input === "Human") {
+            return new Human(player);
+        }
+        else if (input === "First AI Player") {
+            return new RandomAI(player);
+        }
+        else if (input === "Second AI Player") {
+            return new KillerAI(player);
+        }
+        else if (input === "Third AI Player") {
+            return new DynamicAI(player);
+        }
+    }
+}
