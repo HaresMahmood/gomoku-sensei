30af4da2b2b11e7908efcd4763d7d6fd96a5e04a
Hares Mahmood <hmahmood229@gmail.com>
2022-03-16 20:58:00 +0000

    Add [Mocha](https://mochajs.org/) testing framework.

---
+++ tests/gomoku.test.ts
@@ -0,0 +1,10 @@
+import Gomoku from "../src/ts/model/game";
+
+describe("Typescript usage suite", () => {
+  it("should be able to execute a test", () => {
+    equal(true, true);
+  });
+  it("should return expected string", () => {
+    equal(index("incoming"), "incoming-static");
+  });
+});
