6531462b4c826f7a1907f1f89ba68079d670bd9a
Hares Mahmood <hmahmood229@gmail.com>
2022-03-29 10:29:35 +0100

    Update service worker.

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: './',
 	globPatterns: [
-		'**/*.{js,html,webmanifest,json,md,mp3,png,css}'
+		'**/*.{js,html,webmanifest,mp3,png,css}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
